9e2230b408ce6a95018867aad8f5dcedeaf2c4df
marty <bkhwang82@gmail.com>
2019-09-12 10:31:01 +0900

    Add project file for Carthage.

---
+++ Codextended/Codextended.h
@@ -0,0 +1,19 @@
+//
+//  Codextended.h
+//  Codextended
+//
+//  Created by Byung Kook Hwang on 12/09/2019.
+//  Copyright © 2019 Byungkook Hwang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Codextended.
+FOUNDATION_EXPORT double CodextendedVersionNumber;
+
+//! Project version string for Codextended.
+FOUNDATION_EXPORT const unsigned char CodextendedVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Codextended/PublicHeader.h>
+
+
